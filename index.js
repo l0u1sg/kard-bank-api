7f8064a253af025c5697622a3423377cb3ca5caa
Louis GALLET <louis.gallet@tumo.world>
2021-03-25 08:05:17 +0000

    start login resquest

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ const Axios = require('axios')
 exports.printMsg = function () {
     console.log("This is a message from the demo package");
 }
+
+exports.login = function (number, accessCode) {
+    Axios.post('https://api.kard.eu/graphql', {
+        "operationName": "InitSession",
+        "variables": {
+            "input": {
+                "phoneNumber": number,
+                "platform": "ANDROID",
+                "vendorIdentifier": "ghrlt:is-that-a-uuid",
+                "createUser": true
+            }
+        }
+    }).then(function (response) {
+        console.log(response);
+    })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
