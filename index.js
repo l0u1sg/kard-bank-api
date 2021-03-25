4927a4ea28543b9b87a969e0db4ec4885069ce29
Louis GALLET <louis.gallet@tumo.world>
2021-03-25 08:10:19 +0000

    send OPT sms

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ exports.printMsg = function () {
 
 exports.login = function (number, accessCode) {
     Axios.post('https://api.kard.eu/graphql', {
-        "operationName": "InitSession",
-        "variables": {
-            "input": {
-                "phoneNumber": number,
-                "platform": "ANDROID",
-                "vendorIdentifier": "ghrlt:is-that-a-uuid",
-                "createUser": true
+        operationName: "InitSession",
+        variables: {
+            input: {
+                phoneNumber: number,
+                platform: "ANDROID",
+                vendorIdentifier: "android:is-that-a-uuid",
+                createUser: true
             }
-        }
+        },
+        query: "mutation InitSession($input: InitSessionInput!) {\n  initSession(input: $input) {\n    challenge\n    expiresAt\n    errors {\n      message\n      path\n      __typename\n    }\n    __typename\n  }\n}\n"
     }).then(function (response) {
         console.log(response);
     })
